9f5ef3fb650952d4686b8b0dfc0f239d3591397e
krushnashende80 <krushnashende80@gmail.com>
2026-02-15 23:41:08 +0530

    Update Main.java

--- Car-Rental-System/Main.java
+++ Car-Rental-System/Main.java
@@ -6,7 +6,7 @@ class Car {
     private String carId;
     private String brand;
     private String model;
-    private double basePricePerDay;
+    private double basePricePerDay; 
     private boolean isAvailable;
 
     public Car(String carId, String brand, String model, double basePricePerDay) {
